7fd748edb062382232b3b74496e2f4217a9ce4e4
JIIL <romana9059@gmail.com>
2024-05-17 22:37:03 +0300

    SRC

---
+++ src/snake.h
@@ -0,0 +1,36 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+#include <QList>
+#include <QPoint>
+#include <QPainter>
+
+class Snake {
+public:
+    enum class Direction { Left, Right, Up, Down };
+
+    Snake();
+    void move();
+    void grow();
+    void setDirection(Direction newDirection);
+    void draw(QPainter &painter);
+    bool checkSelfCollision() const;
+    void reset();
+    QPoint getHeadPosition() const;
+
+    static int getFIELD_WIDTH();
+
+    static int getFIELD_HEIGHT();
+
+    Direction getDirection() const;
+
+private:
+    QList<QPoint> body;
+    Direction direction;
+    void updateBody();
+
+    static constexpr int FIELD_WIDTH = 600;
+    static constexpr int FIELD_HEIGHT = 600;
+};
+
+#endif // SNAKE_H
